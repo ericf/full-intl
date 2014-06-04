4d0bf8f052dec7a89c1e640f84f924108bf3618f
Andrew Wooldridge <triptych@gmail.com>
2014-06-04 12:26:49 -0700

    Fixing currency formatter and adding number formatting for de-DE

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,24 @@ exports.index = function (req, res) {
         locale  = req.acceptsLanguage(locales) || app.get('default locale');
 
     res.render('index', {
+
         intl: {
             locale  : locale,
-            messages: require('../i18n/' + locale)
+            messages: require('../i18n/' + locale),
+            formats: {
+                number: {
+                    usd: { style: 'currency', currency: 'USD' }
+                }
+            }
         },
 
         user: {
             firstName: 'Anthony',
             lastName : 'Pipkin',
             numBooks : 2000,
-            dateBooks: new Date()
+            dateBooks: new Date(),
+            price    : 20.00
+
         }
     });
 };
